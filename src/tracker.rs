24de9a8bcbb6905ba5f9c23d9d5a809260a76f56
JivkoNushev <jivkon1410@gmail.com>
2023-10-15 01:28:44 +0200

    created create_peer_id function

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,3 +1,5 @@
+use std::num::ParseIntError;
+
 use crate::torrent_file::{BencodedValue, parse_to_torrent_file, Sha1Hash, parse_tracker_response};
 
 use reqwest;
@@ -9,24 +11,37 @@ fn sha1_hash(value: Vec<u8>) -> Sha1Hash {
     Sha1Hash::new(&hasher.finalize())
 }   
 
+fn create_peer_id(id: String) -> String {
+    let padding_len = 20 - id.len();
+    let mut peer_id = id.clone();
+    let padding: String = vec!['C';padding_len].into_iter().collect();
+    peer_id.push_str(&padding);
+    println!("peer id {peer_id}");
+    peer_id
+}
+
 // TODO: Change to Result type insted of Option
 fn create_tracker_url(torrent_file: &mut BencodedValue) -> Option<String> {
-    let info_value = torrent_file.get_from_dict("info");
+    if let BencodedValue::String(tracker_announce) = torrent_file.get_from_dict("announce") {
+        if let BencodedValue::Dict(info_dict) = torrent_file.get_from_dict("info") {
+            let bencoded_info_dict = parse_to_torrent_file(&torrent_file.get_from_dict("info"));
+            let hashed_dict_url_encoded = sha1_hash(bencoded_info_dict).as_url_encoded();
 
-    let bencoded_info_dict = parse_to_torrent_file(&info_value);
+            let mut tracker_url = tracker_announce;
 
-    let hashed_dict = sha1_hash(bencoded_info_dict);
+            tracker_url.push_str("?info_hash=");
+            tracker_url.push_str(&hashed_dict_url_encoded);
 
-    let hashed_dict_url_encoded = hashed_dict.as_url_encoded();
+            tracker_url.push_str("&peer_id=");
+            tracker_url.push_str(create_peer_id("GEY".to_string()).as_str());
 
-    let tracker_announce: BencodedValue = torrent_file.get_from_dict("announce");
-
-    if let BencodedValue::String(s) = tracker_announce {
-        let mut tracker_url = String::from(s);
-        tracker_url.push_str("?info_hash=");
-        tracker_url.push_str(&hashed_dict_url_encoded);
-        tracker_url.push_str("&peer_id=AAAAAAAAAAAAAAAAAAAA&port=6881&uploaded=0&downloaded=0&left=0&compact=1&event=started");
-        Some(tracker_url)
+            tracker_url.push_str("&port=6881&uploaded=0&downloaded=0&left=0&compact=1&event=started");
+            Some(tracker_url)
+        }
+        else {
+            None
+        }
+        
     }
     else {
         None
@@ -69,4 +84,5 @@ pub fn get_peers(torrent_file: &mut BencodedValue) {
     let result_dict = parse_tracker_response(&a);
 
     println!("{:?}", result_dict);
+
 }
