fb47221b17f3019de7a5e15c367934a65e90085b
JivkoNushev <jivkon1410@gmail.com>
2023-10-15 19:21:23 +0200

    connecting to peers

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,46 @@ mod torrent_file;
 mod utils;
 mod tracker;
 
-use tracker::get_peers;
+use byteorder::BE;
+use tokio::sync::mpsc;
 
-use crate::torrent_file::{
-    BencodedValue,
-    parse_torrent_file, 
-    read_file_as_bytes,
-    parse_to_torrent_file
+use tracker::{get_peers};
+
+use crate::{
+    torrent_file:: {
+        BencodedValue,
+        parse_torrent_file, 
+        read_file_as_bytes,
+        parse_to_torrent_file,
+    }, 
+    tracker:: {
+        Peer, 
+        FileSaver,
+    },
 };
 
-fn main() {
+pub async fn download(torrent_file: BencodedValue, peers: Vec<Peer>, mut file_saver: FileSaver) {
+    match tokio::join!(
+        file_saver.start(),
+        tokio::spawn(async move {
+            for peer in peers {
+                let torrent_file_clone = torrent_file.clone();
+                tokio::spawn(async move {
+                    peer.download(torrent_file_clone).await;
+                });
+            }
+        }),
+    ) {
+        ((), Ok(_)) => println!("File downloaded successfully"),
+        ((), Err(e)) => println!("Error downloading file: {:?}", e),
+    };
+}
+
+#[tokio::main]
+async fn main() {
 
     // Read the torrent file into a byte array
-    let torrent_data: Vec<u8> = match read_file_as_bytes("torrent_files/centOS.torrent") {
+    let torrent_file: Vec<u8> = match read_file_as_bytes("torrent_files/centOS.torrent") {
         Ok(data) => data,
         Err(e) => {
             println!("Error reading torrent file: {:?}", e);
@@ -22,19 +49,18 @@ fn main() {
         }
     };
 
-    // println!("{:?}", torrent_data);
-
-
     // Parse the torrent file
+    let mut torrent_file: BencodedValue = parse_torrent_file(&torrent_file);
 
-    let mut torrent_file: BencodedValue = parse_torrent_file(&torrent_data);
+    // Create a channel fot the file saver
+    let (tx, rx) = mpsc::channel::<Vec<u8>>(100);
 
-    // println!("{:?}", torrent_file);
+    // Create a file saver instance 
+    let file_saver = FileSaver::new("newfile".to_string(), rx);
 
-
-    // Get the tracker info
-    let peers = get_peers(&mut torrent_file);
-
-    println!("{:?}", peers);
+    // get the initial peers
+    let peers = get_peers(&mut torrent_file, tx).await;
+    // println!("{:?}", peers);
     
+    download(torrent_file, peers, file_saver).await;
 }
--- src/tracker.rs
+++ src/tracker.rs
@@ -4,6 +4,7 @@ use crate::torrent_file::{BencodedValue, PeerAddress, parse_to_torrent_file, Sha
 
 use reqwest;
 use sha1::{Sha1, Digest};
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
 
 fn sha1_hash(value: Vec<u8>) -> Sha1Hash {
     let mut hasher = Sha1::new();
@@ -33,7 +34,7 @@ fn create_tracker_url(torrent_file: &mut BencodedValue) -> Option<String> {
             tracker_url.push_str(&hashed_dict_url_encoded);
 
             tracker_url.push_str("&peer_id=");
-            tracker_url.push_str(create_peer_id("GEY".to_string()).as_str());
+            tracker_url.push_str(create_peer_id("1".to_string()).as_str());
 
             tracker_url.push_str("&port=6881&uploaded=0&downloaded=0&left=0&compact=1&event=started");
             Some(tracker_url)
@@ -48,7 +49,36 @@ fn create_tracker_url(torrent_file: &mut BencodedValue) -> Option<String> {
     }
 }
 
-#[derive(Default, Debug)]
+pub struct FileSaver {
+    file_name: String,
+    file_queue: mpsc::Receiver<Vec<u8>>,
+}
+
+impl FileSaver {
+    pub fn new(file_name: String, file_queue_rx: mpsc::Receiver<Vec<u8>>) -> FileSaver {
+        FileSaver { file_name: file_name, file_queue: file_queue_rx }
+    }
+
+    pub async fn start(&mut self) {
+        loop {
+            let piece = self.file_queue.recv().await;
+
+            if let Some(piece) = piece {
+                self.save(piece).await;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    async fn save(&self, piece: Vec<u8>) {
+        let mut file = tokio::fs::File::create(self.file_name.clone()).await.unwrap();
+        file.write_all(&piece).await.unwrap(); // ??
+    }
+}
+    
+#[derive(Debug)]
 pub struct Peer {
     id: String,
     address: String,
@@ -57,20 +87,40 @@ pub struct Peer {
     am_interested: bool,
     peer_choking: bool,
     peer_interested: bool,
+    file_queue: mpsc::Sender<Vec<u8>>,
 }
 
+use tokio::sync::mpsc;
+
 impl Peer {
-    pub fn new(peer_address: PeerAddress, id: String) -> Peer {
-        Peer { id: id, address: peer_address.get_ip().clone(), port: peer_address.get_port().clone(), am_choking: true, am_interested: true, peer_choking: true, peer_interested: true }
+    pub fn new(peer_address: PeerAddress, id: String, file_queue_tx: mpsc::Sender<Vec<u8>>) -> Peer {
+        Peer { 
+            id: id, 
+            address: peer_address.get_ip().clone(), 
+            port: peer_address.get_port().clone(), 
+            am_choking: false, 
+            am_interested: false, 
+            peer_choking: true, 
+            peer_interested: false,
+            file_queue: file_queue_tx,
+        }
     }
     
-    pub fn choke(&mut self) {
-        self.am_choking = true;
-    }
+    // pub fn choke(&mut self) {
+    //     self.am_choking = true;
+    // }
 
-    pub fn unchoke(&mut self) {
-        self.am_choking = false;
-    }
+    // pub fn unchoke(&mut self) {
+    //     self.am_choking = false;
+    // }
+
+    // pub fn peer_interest(&mut self) {
+    //     self.peer_interested = true;
+    // }
+
+    // pub fn peer_uninterest(&mut self) {
+    //     self.peer_interested = false;
+    // }
 
     pub fn interest(&mut self) {
         self.am_interested = true;
@@ -88,21 +138,82 @@ impl Peer {
         self.peer_choking = false;
     }
 
-    pub fn peer_interest(&mut self) {
-        self.peer_interested = true;
+    async fn handshake(&self, stream: &mut tokio::net::TcpStream, bencoded_info_dict: BencodedValue) {
+        // TODO: return a result type for get_from_dict
+        let bencoded_info_dict = parse_to_torrent_file(&bencoded_info_dict.get_from_dict("info"));
+        let mut hashed_dict_url_encoded = sha1_hash(bencoded_info_dict).get_hash_ref().to_vec();
+
+        // create handshake
+        let mut handshake: Vec<u8> = Vec::new();
+        handshake.push(19); // len of the protocol
+        handshake.append(&mut "BitTorrent protocol".as_bytes().to_vec());
+        handshake.append(&mut vec![0 as u8; 8]);
+        handshake.append(&mut hashed_dict_url_encoded);
+        handshake.append(&mut self.id.as_bytes().to_vec());
+
+        // send handshake
+        stream.write_all(&handshake).await.unwrap();
+
+        // receive handshake
+        let mut handshake_response = vec![0; 68];
+        match stream.read_exact(&mut handshake_response).await {
+            Ok(_) => {},
+            Err(e) => {
+                println!("Error reading handshake response: {:?}", e);
+                return;
+            }
+        }
+
+        println!("Handshake response: {:?}", handshake_response);
+
+        loop {
+            let message = vec![0 as u8; 4];
+            // send keep alive message
+            stream.write_all(&message).await.unwrap();
+            println!("Keep alive: {:?}", message);
+    
+            // receive keep alive message
+            let mut keep_alive_response = vec![0; 4];
+            stream.read_exact(&mut keep_alive_response).await.unwrap();
+    
+            println!("Keep alive response: {:?}", keep_alive_response);
+    
+            // wait 100 seconds
+            tokio::time::sleep(tokio::time::Duration::from_secs(100)).await;
+        }
+
+
     }
 
-    pub fn peer_uninterest(&mut self) {
-        self.peer_interested = false;
+    pub async fn download(&self, bencoded_info_dict: BencodedValue) {
+        // create a tcp connection to the peer
+        loop {
+            let mut stream = tokio::net::TcpStream::connect(format!("{}:{}", self.address, self.port)).await;
+
+            match stream {
+                Ok(mut stream) => {
+                    println!("Connected to peer: {}", self.address);
+                    self.handshake(&mut stream, bencoded_info_dict.clone()).await;
+                    break;
+                },
+                Err(e) => {
+                    // println!("Error connecting to peer:{}: {}", self.id, e);
+                    continue;
+                }
+            }
+        }
+        
+
+        // start exchanging messages
     }
 }
 
-pub fn get_peers(torrent_file: &mut BencodedValue) -> Vec<Peer> {
+pub async fn get_peers(torrent_file: &mut BencodedValue, file_queue_tx: mpsc::Sender<Vec<u8>>) -> Vec<Peer> {
     let url = create_tracker_url(torrent_file).unwrap();
 
-    let resp = reqwest::blocking::get(url).unwrap();
+    let resp = reqwest::get(url).await.unwrap();
 
-    let bencoded_response = resp.bytes().unwrap();
+    let bencoded_response = resp.bytes().await.unwrap();
 
     let bencoded_dict = parse_tracker_response(&bencoded_response);
     
@@ -110,13 +221,10 @@ pub fn get_peers(torrent_file: &mut BencodedValue) -> Vec<Peer> {
 
     if let BencodedValue::Dict(dict) = bencoded_dict {
         if let BencodedValue::ByteAddresses(byte_addresses) = dict.get("peers").unwrap() {
-            println!("HERE!");
             for i in 0..20 {
                 if let Some(addr) = byte_addresses.get(i) {
-                    println!("HERE{i}!");
-    
-                    let peer = Peer::new(addr.clone(), i.to_string());
-                peer_array.push(peer);
+                    let peer = Peer::new(addr.clone(), i.to_string(), file_queue_tx.clone());
+                    peer_array.push(peer);
                 }
             }
         }
